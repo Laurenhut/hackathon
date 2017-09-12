1a8d12e24b79511d31b126837629ea323e15ce64
rikago <l.hutson@live.com>
2017-09-12 00:13:24 -0500

    this contains the three python programs worked on during the hackathon.

---
+++ challeng2.py
@@ -0,0 +1,20 @@
+lights= [False]*5
+person_num=1
+length= len(lights)
+iterations =0
+
+while person_num <= length:
+	person_num
+	iterations_done=0
+	iterations =length/person_num
+
+	int (iterations)
+
+	#while iterations_done <iterations:
+	#	print iterations_done
+	#	lightval=(iterations_done*person_num)
+		#print lightval
+	#	lights[lightval]=not lights[lightval]	
+	#	iterations_done+=1
+	#person_num+=1
+	print lights
